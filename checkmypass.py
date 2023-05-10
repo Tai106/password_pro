c933f41c42e1cfad621bb9c2f3e6722e6ce1cb09
Gravity <tevoned@gmail.com>
2023-05-09 20:28:26 -0400

    Initial commit

---
+++ checkmypass.py
@@ -0,0 +1,19 @@
+import requests
+import hashlib
+
+#  funtion to request API data 
+def request_api_data(query_char):
+    
+    url = 'https://api.pwnedpasswords.com/range/' + query_char
+    res = requests.get(url)
+    if res.status_code != 200:
+        raise RuntimeError(f'Error fetching: {res.status_code}, check the api and try again')
+    return res
+
+    
+
+def pwned_api_data(password):
+    # check password if it exists in API response
+    sha1password = hashlib
+
+request_api_data('123')
