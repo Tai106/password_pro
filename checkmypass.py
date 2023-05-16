ddb48d27c527215ead3d5a9fb47f6d1ad9c500c4
Gravity <tevoned@gmail.com>
2023-05-15 21:38:21 -0400

    password debug

--- checkmypass.py
+++ checkmypass.py
@@ -1,16 +1,18 @@
 import requests
 import hashlib
-import sys 
+import sys
 
-#  funtion to request API data 
+#  funtion to request API data
+
+
 def request_api_data(query_char):
-    
+
     url = 'https://api.pwnedpasswords.com/range/' + query_char
     res = requests.get(url)
     if res.status_code != 200:
-        raise RuntimeError(f'Error fetching: {res.status_code}, check the api and try again')
+        raise RuntimeError(
+            f'Error fetching: {res.status_code}, check the api and try again')
     return res
-
 
 
 def get_password_leaks_count(hashes, hash_to_check):
@@ -18,25 +20,26 @@ def get_password_leaks_count(hashes, hash_to_check):
     for h, count in hashes:
         if h == hash_to_check:
             return count
-        return 0 
+    return 0
 
-    
 
-def pwned_api_data(password):
+def pwned_api_check(password):
     # check password if it exists in API response
     sha1password = (hashlib.sha1(password.encode('utf-8')).hexdigest().upper())
     first5_char, tail = sha1password[:5], sha1password[5:]
     response = request_api_data(first5_char)
-    print(response)
     return get_password_leaks_count(response, tail)
 
+
 def main(args):
-    for password in arg:
+    for password in args:
         count = pwned_api_check(password)
         if count:
-            print(f"{password} was found {count} times... you shoud change your password immediately!")
+            print(
+                f"{password} was found {count} times... you shoud change your password immediately!")
         else:
             print(f'{password} was not found.')
-        return 'done!'
+    return 'done!'
+
 
 main(sys.argv[1:])
